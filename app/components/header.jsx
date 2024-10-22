7aca1605d045661242c36e2e4404edef03a0b9fc
maurine-hub <maurine780@gmail.com>
2024-10-22 08:14:54 +0100

    gestion du header et modification du hero

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,13 +1,106 @@
 
-const Header = () =>{
-  return(
-    <>
-      <div className="absolute bg-white">
+// const Header = () =>{
+//   return(
+//     <>
+//       <div className="absolute bg-white">
 
+//       </div>
+//     </>
+//   )
+
+// }
+
+// export default Header;
+
+'use client'
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { FaFacebook, FaTwitter, FaYoutube, FaEnvelope, FaPhone, FaBars } from 'react-icons/fa';
+import Image from 'next/image';
+// import { images } from '../lib/images';
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className=" absolute z-50 mx-4 max-sm:mt-6 sm:mx-24 top-5 left-0 right-0">
+      {/* Top bar (hidden on small screens) */}
+      <div className="hidden md:flex bg-transparent p-4 items-center justify-between">
+        <div className="flex justify-between w-[30%] text-xl">
+          <div className="flex items-center text-white">
+            <FaEnvelope />
+            <p className="ml-3 text-lg">exemple@gmail.com</p>
+          </div>
+          <div className="flex items-center text-white">
+            <FaPhone />
+            <p className="ml-3 text-lg">xxx xxx xxx</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between w-[15%] text-white text-xl">
+          <FaFacebook />
+          <FaTwitter />
+          <FaYoutube />
+        </div>
       </div>
-    </>
-  )
 
-}
+      {/* Main navigation */}
+      <div className="w-full bg-white shadow-md rounded-md p-2">
+        <div className="container mx-auto md:px-6 md:py-1 flex items-center justify-between ">
+          {/* Burger menu for mobile */}
+          <button
+            className="md:hidden text-black text-2xl"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <FaBars />
+          </button>
+
+          {/* Desktop Menu */}
+          <nav className="hidden  md:flex">
+            <ul className="flex space-x-4 uppercase text-black font-semibold ">
+              <li><Link href="/" className="text-[#cc1616] underline">Home</Link></li>
+              <li><Link href="#about" className="hover:text-[#cc1616]">About</Link></li>
+              <li><Link href="#features" className="hover:text-[#cc1616]">Features</Link></li>
+            </ul>
+          </nav>
+
+          {/* Logo */}
+          <Link href="/">
+            <div className="max-sm:w-28 absolute max-sm:top-[-10px] top-0 transform md:translate-y-1/2 left-1/2 md:w-44 md:left-[37rem] bg-white py-6 px-3 shadow-xl rounded-md -translate-x-1/2">
+              <Image src='/images/logoIpercash.png' alt="Logo" width={500} height={300} />
+            </div>
+          </Link>
+          <button className='bg-[#cc1616] p-2 rounded-md text-white text-sm uppercase md:hidden'>download</button>
+
+
+          {/* Download button */}
+          <nav className="hidden md:flex items-center">
+            <ul className="flex space-x-4 uppercase text-black font-semibold pr-6">
+              <li><Link href="/screenshot" className="hover:text-gray-900">Screenshot</Link></li>
+              <li><Link href="/contact" className="hover:text-gray-900">Contact</Link></li>
+            </ul>
+            <button className="bg-[#cc1616] py-2 px-6 rounded-md text-white uppercase font-semibold">
+              Download
+            </button>
+          </nav>
+        </div>
+      </div>
+
+      {/* Mobile Menu (visible on small screens when menuOpen is true) */}
+      {menuOpen && (
+        <nav className="md:hidden bg-white shadow-md rounded-md mt-2">
+          <ul className="flex flex-col space-y-2 p-4 uppercase text-black font-semibold">
+            <li><Link href="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+            <li><Link href="#about" onClick={() => setMenuOpen(false)}>About</Link></li>
+            <li><Link href="#features" onClick={() => setMenuOpen(false)}>Features</Link></li>
+            <li><Link href="/screenshot" onClick={() => setMenuOpen(false)}>Screenshot</Link></li>
+            <li><Link href="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+};
 
 export default Header;
--- app/pages/hero.jsx
+++ app/pages/hero.jsx
@@ -5,28 +5,30 @@ import Image from 'next/image';
 const Hero = ()=>{
   return(
     <>
-      <div className="bg-[#cc1616] w-full px-5 md:px-32">
-        <div className="flex w-full max-sm:flex-col py-20 md:mx-16  ">
-          <div className="relative border-8 border-red-500 w-full h-[27rem] md:w-1/2">
-            <div className="flex flex-col absolute right-4 top-2 bg-white w-full p-6">
+      <div className="bg-[#cc1616] w-full px-5 md:p-32">
+        <div className="flex w-full max-sm:flex-col max-sm:py-40 z-20 md:mt-24 md:items-center ">
+          <div className="relative border-8 border-red-500 w-full h-[27rem] md:w-1/2 md:h-[29rem] ">
+            <div className="flex flex-col absolute right-4 top-2 bg-white w-full px-6 pb-2 pt-6 md:right-8">
               <h1 className="uppercase text-[#cc1616] text-2xl font-bold md:text-4xl">a great app makes your life better</h1>
               <p className="py-4 text-neutral-500 md:py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dignissimos, corporis necessitatibus ab maiores dolores ea dolorem quisquam dicta voluptatum cumque vitae explicabo velit, voluptate sunt. Ex soluta magni aspernatur?</p>
               <p className="font-semibold uppercase text-xl md:text-2xl">download app now</p>
               <div className='flex items-center '>
-                <div>
+                <div className='w-1/2'>
                   <Image
                     src='/images/btn1.png'
                     alt=''
                     width={110}
                     height={32}
+                    className='w-[90%] md:w-full'
                   />
                 </div>
-                <div>
+                <div className='w-1/2'>
                   <Image
                     src='/images/btn2.png'
                     alt=''
-                    width={110}
+                    width={200}
                     height={32}
+                    className='w-[90%] md:w-full'
                   />
                 </div>
               </div>
@@ -38,7 +40,7 @@ const Hero = ()=>{
               src='/images/second.png'
               alt=''
               width={900}
-              height={500}
+              height={900}
             />
           </div>
         </div>
